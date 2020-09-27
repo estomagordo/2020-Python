bdbab23fa40d56977f43bc403b645b65f38f2b3e
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-09-27 16:54:22 +0200

    ending only active games :)

--- main.py
+++ main.py
@@ -78,11 +78,12 @@ def interactive():
 
 def endall():
     games = game_layer.get_games()
-
+    print('Ending active games.')
+    
     for game in games:
-        game_layer.end_game(game['gameId'])
-        print('Deleted', game['gameId'])
-
+        if game['active']:
+            game_layer.end_game(game['gameId'])
+            print('Ended, game['gameId'])
 
 def main():
     if len(argv) < 2:
@@ -120,11 +121,14 @@ def take_turn():
     x = -1
     y = -1
 
-    if len(state.residences) < 3:
+    maxrescount = 3
+    curres = 0
+
+    if len(state.residences) < maxrescount:
         for i in range(len(state.map)):
             if x > -1:
                 break
-            for j in range(len(state.map)):
+            for j in range(len(state.map[i])):
                 if state.map[i][j] == 0:
                     x = i
                     y = j
@@ -132,25 +136,28 @@ def take_turn():
 
         command = game_layer.place_foundation((x, y), game_layer.game_state.available_residence_buildings[0].building_name)
     else:
-        the_only_residence = state.residences[0]
-        if the_only_residence.build_progress < 100:
-            command = game_layer.build((the_only_residence.X, the_only_residence.Y))
-        elif the_only_residence.health < 50:
-            command = game_layer.maintenance((the_only_residence.X, the_only_residence.Y))
-        elif the_only_residence.temperature < 18:
-            blueprint = game_layer.get_residence_blueprint(the_only_residence.building_name)
-            energy = blueprint.base_energy_need + 0.5 \
-                     + (the_only_residence.temperature - state.current_temp) * blueprint.emissivity / 1 \
-                     - the_only_residence.current_pop * 0.04
-            command =  game_layer.adjust_energy_level((the_only_residence.X, the_only_residence.Y), energy)
-        elif the_only_residence.temperature > 24:
-            blueprint = game_layer.get_residence_blueprint(the_only_residence.building_name)
-            energy = blueprint.base_energy_need - 0.5 \
-                     + (the_only_residence.temperature - state.current_temp) * blueprint.emissivity / 1 \
-                     - the_only_residence.current_pop * 0.04
-            command = game_layer.adjust_energy_level((the_only_residence.X, the_only_residence.Y), energy)
-        elif state.available_upgrades[0].name not in the_only_residence.effects:
-            command =  game_layer.buy_upgrade((the_only_residence.X, the_only_residence.Y), state.available_upgrades[0].name)
+        residence = state.residences[curres]
+
+        if residence.build_progress < 100:
+            command = game_layer.build((residence.X, residence.Y))
+        elif curres < maxrescount - 1:
+            curres += 1
+        # elif residence.health < 50:
+        #     command = game_layer.maintenance((residence.X, residence.Y))
+        # elif residence.temperature < 18:
+        #     blueprint = game_layer.get_residence_blueprint(residence.building_name)
+        #     energy = blueprint.base_energy_need + 0.5 \
+        #              + (residence.temperature - state.current_temp) * blueprint.emissivity / 1 \
+        #              - residence.current_pop * 0.04
+        #     command =  game_layer.adjust_energy_level((residence.X, residence.Y), energy)
+        # elif residence.temperature > 24:
+        #     blueprint = game_layer.get_residence_blueprint(residence.building_name)
+        #     energy = blueprint.base_energy_need - 0.5 \
+        #              + (residence.temperature - state.current_temp) * blueprint.emissivity / 1 \
+        #              - residence.current_pop * 0.04
+        #     command = game_layer.adjust_energy_level((residence.X, residence.Y), energy)
+        # elif state.available_upgrades[0].name not in residence.effects:
+        #     command =  game_layer.buy_upgrade((residence.X, residence.Y), state.available_upgrades[0].name)
         else:
             command = game_layer.wait()
 
