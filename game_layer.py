bfcb513ff6817e4bdc23a7cd61b4618338c2c270
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-09-27 11:50:02 +0200

    so even the default autoplay should record its steps

--- game_layer.py
+++ game_layer.py
@@ -23,28 +23,42 @@ class GameLayer:
         foundation = {'Position': position, 'BuildingName': building_name}
         self.game_state.update_state(self.api.place_foundation(foundation, self.game_state.game_id))
 
+        return 'place_foundation ' + building_name
+
     def build(self, pos):
         position = {'position': {'X': pos[0], 'Y': pos[1]}}
         self.game_state.update_state(self.api.build(position, self.game_state.game_id))
+
+        return 'build ' + str(pos[0]) + ',' + str(pos[1])
 
     def maintenance(self, pos):
         position = {'position': {'x': pos[0], 'y': pos[1]}}
         self.game_state.update_state(self.api.maintenance(position, self.game_state.game_id))
 
+        return 'maintenance' + str(pos[0]) + ',' + str(pos[1])
+
     def demolish(self, pos):
         position = {'position': {'x': pos[0], 'y': pos[1]}}
         self.game_state.update_state(self.api.demolish(position, self.game_state.game_id))
+
+        return 'demolish' + str(pos[0]) + ',' + str(pos[1])
 
     def adjust_energy_level(self, pos, value):
         position = {'x': pos[0], 'y': pos[1]}
         self.game_state.update_state(self.api.adjust_energy({'position': position, 'value': value}, self.game_state.game_id))
 
+        return 'adjust_energy_level' + str(pos[0]) + ',' + str(pos[1])
+
     def wait(self):
         self.game_state.update_state(self.api.wait(self.game_state.game_id))
+
+        return 'wait'
 
     def buy_upgrade(self, pos, upgrade):
         position = {'x': pos[0], 'y': pos[1]}
         self.game_state.update_state(self.api.buy_upgrades({'position': position, 'upgradeAction': upgrade}, self.game_state.game_id))
+
+        return 'buy_upgrade' + str(pos[0]) + ',' + str(pos[1]) + upgrade
 
     def get_score(self):
         return self.api.get_score(self.game_state.game_id)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import glob
 import os
 
 from sys import argv
+from time import time
 
 from game_layer import GameLayer
 
@@ -16,15 +17,27 @@ map_name = 'training1'  # TODO: You map choice here. If left empty, the map 'tra
 game_layer = GameLayer(api_key)
 
 
+def write(filename, commands):
+    with open(filename) as f:
+        for command in commands:
+            f.write(command)
+
+
 def simple():
+    filename = '\\games\\' +  map_name + str(time()).split('.')[0]
+    commands = []
+
     game_layer.new_game(map_name)
     print('Starting game: ' + game_layer.game_state.game_id)
     game_layer.start_game()
+
     while game_layer.game_state.turn < game_layer.game_state.max_turns:
-        take_turn()
+        commands.append(take_turn())
+
     print('Done with game: ' + game_layer.game_state.game_id)
     print('Final score was: ' + str(game_layer.get_score()['finalScore']))
 
+    write(filename, commands)
 
 def replay(filename):
     commands = []
@@ -89,6 +102,9 @@ def take_turn():
     # TODO The following is a short example of how to use the StarterKit
 
     state = game_layer.game_state
+
+    command = ''
+
     if len(state.residences) < 1:
         for i in range(len(state.map)):
             for j in range(len(state.map)):
@@ -96,34 +112,38 @@ def take_turn():
                     x = i
                     y = j
                     break
-        game_layer.place_foundation((x, y), game_layer.game_state.available_residence_buildings[0].building_name)
+
+        command = game_layer.place_foundation((x, y), game_layer.game_state.available_residence_buildings[0].building_name)
+
     else:
         the_only_residence = state.residences[0]
         if the_only_residence.build_progress < 100:
-            game_layer.build((the_only_residence.X, the_only_residence.Y))
+            command = game_layer.build((the_only_residence.X, the_only_residence.Y))
         elif the_only_residence.health < 50:
-            game_layer.maintenance((the_only_residence.X, the_only_residence.Y))
+            command = game_layer.maintenance((the_only_residence.X, the_only_residence.Y))
         elif the_only_residence.temperature < 18:
             blueprint = game_layer.get_residence_blueprint(the_only_residence.building_name)
             energy = blueprint.base_energy_need + 0.5 \
                      + (the_only_residence.temperature - state.current_temp) * blueprint.emissivity / 1 \
                      - the_only_residence.current_pop * 0.04
-            game_layer.adjust_energy_level((the_only_residence.X, the_only_residence.Y), energy)
+            command =  game_layer.adjust_energy_level((the_only_residence.X, the_only_residence.Y), energy)
         elif the_only_residence.temperature > 24:
             blueprint = game_layer.get_residence_blueprint(the_only_residence.building_name)
             energy = blueprint.base_energy_need - 0.5 \
                      + (the_only_residence.temperature - state.current_temp) * blueprint.emissivity / 1 \
                      - the_only_residence.current_pop * 0.04
-            game_layer.adjust_energy_level((the_only_residence.X, the_only_residence.Y), energy)
+            command = game_layer.adjust_energy_level((the_only_residence.X, the_only_residence.Y), energy)
         elif state.available_upgrades[0].name not in the_only_residence.effects:
-            game_layer.buy_upgrade((the_only_residence.X, the_only_residence.Y), state.available_upgrades[0].name)
+            command =  game_layer.buy_upgrade((the_only_residence.X, the_only_residence.Y), state.available_upgrades[0].name)
         else:
-            game_layer.wait()
+            command = game_layer.wait()
+
     for message in game_layer.game_state.messages:
         print(message)
     for error in game_layer.game_state.errors:
         print('Error: ' + error)
 
+    return command
 
 if __name__ == '__main__':
     main()
